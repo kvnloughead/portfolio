d50530d1116e7d54b9e772f18d663f3cfe8abf25
Kevin Loughead <kvnloughead@gmail.com>
2021-05-08 11:03:24 -0400

    refactor: about component into styled component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,21 +1,27 @@
 import * as React from 'react';
 
 import { personalInfo, profilePic } from '../../config/resume-data';
-import { colors } from '../../utils/constants';
-import * as styles from './about.module.css';
+
+import {
+  AboutContainer,
+  Image,
+  Title,
+  Summary,
+  TextContainer,
+} from './styles';
 
 const About = () => (
-  <section id="about" className={styles.about} >
-    <img className={styles.about__image} alt={personalInfo.name} src={profilePic} />
-    <div className={styles.about__textContainer}>
-      <h2 className={styles.about__title} style={{ color: colors.white }}>
+  <AboutContainer id="about" >
+    <Image alt={personalInfo.name} src={profilePic} />
+    <TextContainer>
+      <Title>
         About Me
-      </h2>
-      <p className={styles.about__summary} style={{ color: colors.white }}>
+      </Title>
+      <Summary>
         {personalInfo.summary}
-      </p>
-    </div>
-  </section>
+      </Summary>
+    </TextContainer>
+  </AboutContainer>
 );
 
 export default About;
