28b81c442a7fc53d807baf0d2c8c87fbe0122183
Kevin Loughead <kvnloughead@gmail.com>
2021-11-06 13:06:59 -0400

    add material ui button

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,18 +1,25 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
 
 import Project from '../project/project';
 import { ProjectsContainer, Title, List } from './styles';
 
 const Projects = React.forwardRef((props, ref) => {
+  const [numberShown] = useState(3);
   return (
     <div ref={ref} style={{ width: '100%' }}>
       <ProjectsContainer id="projects">
         <Title>{props.title}</Title>
         <List>
-          {props.projects.map((project) => (
+          {props.projects.slice(0, numberShown).map((project) => (
             <Project key={project.title} data={project} />
           ))}
         </List>
+        {props.projects.length > 3 && (
+          <Button variant="outlined" color="error">
+            See More
+          </Button>
+        )}
       </ProjectsContainer>
     </div>
   );
